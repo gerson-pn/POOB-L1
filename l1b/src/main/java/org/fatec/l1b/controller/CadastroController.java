454fd4048c7e4c3818d480cb1444352c6d59a4c8
gpn <gpn@DESKTOP-73J6ED3.mshome.net>
2021-03-25 11:20:24 -0300

    inclusão de comentários

--- l1b/src/main/java/org/fatec/l1b/controller/CadastroController.java
+++ l1b/src/main/java/org/fatec/l1b/controller/CadastroController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+// comentários para "explicação" do cadastro controller
 @Controller
 public class CadastroController {
 	@Autowired
